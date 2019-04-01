893b5dec00e76c2aa8a39d13ca0ace5e3c05948d
Cesar Gomez <zootechdrum@gmail.com>
2019-04-01 10:00:57 -0700

    added search bar/no functionality yet

--- index.php
+++ index.php
@@ -12,7 +12,27 @@
                 </svg>
             </button>
         </form>
+    
+
+        <nav class="user-nav">
+            <div class="user-nav__icon-box">
+                <svg class="user-nav__icon">
+                    <use xlink:href="<?php echo get_template_directory_uri() ?>/sprite.svg#icon-facebook"></use>
+                </svg>
+            </div>
+            <div class="user-nav__icon-box">
+                <svg class="user-nav__icon">
+                    <use xlink:href="<?php echo get_template_directory_uri() ?>/sprite.svg#icon-github"></use>
+                </svg>
+            </div>
+            <div class="user-nav__icon-box">
+                <svg class="user-nav__icon">
+                    <use xlink:href="<?php echo get_template_directory_uri() ?>/sprite.svg#icon-instagram"></use>
+                </svg>
+            </div>
+        </nav>
     </header>
+
     <div class="content">
             <nav class="sidebar">
                 Hello this is the navbar
