883bc627c2018617fab9b4c43dc11b1ec7e0c7d2
Cesar Gomez <zootechdrum@gmail.com>
2019-03-31 23:31:41 -0700

    added svg and logo for site

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
 <div class="container">
     <header class="header">
         <img src="<?php echo get_template_directory_uri()?>/header_logo.png"<alt="business logo" class="logo" />
+        <form action="#" class="search">
+            <input type="text" class="search__input" placeholder="Search Blogs">
+
+            <button class="search__button">
+                <svg class="search__icon">
+                    <use xlink:href="<?php echo get_template_directory_uri() ?>/sprite.svg#icon-magnifying-glass"></use>
+                </svg>
+            </button>
+        </form>
     </header>
     <div class="content">
             <nav class="sidebar">
